2d33e06e486ba04af0e72071fc91acbe29ee14ae
jem.sai <jem.sai@whitecloak.com>
2018-07-26 19:49:06 +0800

    DroppablePin - Added a class to show an annotation (pin) displayed on map

--- pixel-city/View/DroppablePin.swift
+++ pixel-city/View/DroppablePin.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2018 Jemimah Beryl M. Sai. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import MapKit
+
+class DroppablePin: NSObject, MKAnnotation {
+    dynamic var coordinate: CLLocationCoordinate2D
+    var identifier: String
+    
+    init(coordinate: CLLocationCoordinate2D, identifier: String) {
+        self.coordinate = coordinate
+        self.identifier = identifier
+        super.init()
+    }
+}
