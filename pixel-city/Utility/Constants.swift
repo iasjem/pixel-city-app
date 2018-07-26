3a1ad6f7cb00a51b133a859c77b2b913ea983671
jem.sai <jem.sai@whitecloak.com>
2018-07-26 19:48:20 +0800

    Constants - added function to retrieve a Flickr URL from API

--- pixel-city/Utility/Constants.swift
+++ pixel-city/Utility/Constants.swift
@@ -7,4 +7,9 @@
 //
 
 import Foundation
+
 let API_KEY = "6c02f7479a9a0d0520b7a0ea9b721ff4"
+
+func flickrUrl(forApiKey key: String, withAnnotation annotation: DroppablePin, andNumberOfPhotos number: Int) -> String {
+    return "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=\(key)&lat=\(annotation.coordinate.latitude)&lon=\(annotation.coordinate.longitude)&radius=1&radius_units=mi&per_page=\(number)&format=json&nojsoncallback=1"
+}
